c3a36285d88f992c4429b4549316780b9ca12f46
JenniferMarcia <marciajenifer02@gmail.com>
2024-07-29 12:44:57 +0300

    update test Users

--- Users/tests.py
+++ Users/tests.py
@@ -13,14 +13,20 @@ class CustomUserTests(TestCase):
         self.user = CustomUser.objects.create_user(
             username="testuser", email="test@example.com", password="password"
         )
-        self.client.force_authenticate(user=self.user)
+
+        # Get tokens for the user
+        response = self.client.post(
+            reverse("token_obtain_pair"),
+            {"username": "testuser", "password": "password"},
+        )
+        self.access_token = response.data["access"]
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.access_token}")
 
         # URLs for CustomUser views
         self.user_list_url = reverse("user-list")
         self.user_detail_url = reverse("user-detail", kwargs={"pk": self.user.pk})
         self.user_update_url = reverse("user-update", kwargs={"pk": self.user.pk})
         self.user_delete_url = reverse("user-delete", kwargs={"pk": self.user.pk})
-        self.login_url = reverse("login")
         self.logout_url = reverse("logout")
         self.update_password_url = reverse(
             "update-password", kwargs={"pk": self.user.pk}
@@ -52,21 +58,13 @@ class CustomUserTests(TestCase):
         with self.assertRaises(CustomUser.DoesNotExist):
             CustomUser.objects.get(pk=self.user.pk)
 
-    def test_login(self):
-        data = {"username": self.user.username, "password": "password"}
-        response = self.client.post(self.login_url, data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn("refresh", response.data)
-        self.assertIn("access", response.data)
-        self.assertIn("user", response.data)
-
     def test_logout(self):
         refresh = RefreshToken.for_user(self.user)  # Generate refresh token
         response = self.client.post(
             self.logout_url, {"refresh": str(refresh)}, format="json"
         )
         self.assertEqual(response.status_code, status.HTTP_205_RESET_CONTENT)
-        self.assertIn("Logout successful", response.data["message"])
+        self.assertIn("User logout successfully", response.data["message"])
 
     def test_update_password(self):
         data = {"current_password": "password", "new_password": "new_password"}
